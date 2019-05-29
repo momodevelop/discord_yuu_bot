2fa435d3442a3e2cbd3a20789545f9c6b4fb3cc9
GERALD WONG <ramza88@gmail.com>
2019-05-29 19:39:23 +0800

    updated install.bat

--- work/program/scripts/0_install.bat
+++ work/program/scripts/0_install.bat
@@ -1,7 +1,7 @@
 @echo off
 Pushd "%~dp0"
 
-set MODULES=app-root-path lokijs @types/lokijs discord.js typescript @types/node @types/core-js app-module-path @types/app-module-path request request-promise-native @types/request-promise-native 
+set MODULES=app-root-path@2.2.1 lokijs@1.5.6 @types/lokijs@1.5.2 discord.js@11.4.2 typescript@3.4.3 @types/node@11.13.4 @types/core-js@2.5.0 app-module-path@2.2.0 @types/app-module-path@2.2.0 request@2.88.0 request-promise-native@1.0.7 @types/request-promise-native@1.0.15
 
 cd ..
 echo Uninstalling modules...
